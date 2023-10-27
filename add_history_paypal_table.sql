d95d476215a87f8745825298ad1e9dc0e22b89c0
nguyenthang327 <nguyenducthang327@gmail.com>
2023-10-27 08:11:00 +0700

    sql

---
+++ add_history_paypal_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE historynappaypal (
+    id BIGINT UNSIGNED AUTO_INCREMENT,
+    username VARCHAR(100),
+    tiendo VARCHAR(100),
+		tigia VARCHAR(100),
+		tienviet VARCHAR(100),
+		status VARCHAR(100),
+		date DATETIME,
+		webdinhdanh  VARCHAR(100),
+    PRIMARY KEY (id)
+);
